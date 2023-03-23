6cb860d13c860d0f621853be17fea4f539a9d994
Conall Nelligan <conallnelligan@gmail.com>
2023-03-23 14:48:45 -0400

    almost there

--- main.js
+++ main.js
@@ -1,3 +1,104 @@
+const form = document.querySelector (".form1");
+const fromForm = document.querySelector("#todo-item");
+
+const todoList = document.querySelector(".todolist1");
+
+//const todos = []
+
+const todos = [
+  {
+    text: "item",
+    isDone: false,
+  },
+  {
+
+  }
+]
+
+function drawToDoList() {
+  
+  while (todoList.firstChild){
+    todoList.removeChild(todoList.firstChild);
+  }
+  
+  for (let i = 0; i < todos.length; i++){
+    
+
+    const listItem = document.createElement("li");
+    listItem.textContent = todos[i].text;
+
+    if(todos[i].isDone === true) {
+      listItem.classList.add("done");
+    }
+
+
+    const todoButton = document.createElement("button")
+    todoButton.textContent = "Delete";
+    todoButton.classList.add("todoDelete");
+
+    todoButton.dataset.index = i;
+
+    todoButton.addEventListener("click", deleteTodo);
+
+    const todoDoneButton = document.createElement("button");
+    todoDoneButton.textContent = "Done";
+
+    todoDoneButton.dataset.index = i;
+
+    todoDeleteButton.addEventListener("click", doneTodo);
+
+    listItem.appendChild(todoButton);
+
+    listItem.appendChild(todoDoneButton);
+
+    todoList.appendChild(listItem);
+  }
+}
+
+function deleteTodo(event){
+
+  todoDeleteIndex = event.target.dataset.index;
+
+  todos.splice(todoDeleteIndex, 1);
+
+  drawToDoList();
+}
+
+
+
+function doneTodo(event) {
+  console.log("mark done");
+
+  todoDeleteIndex = event.target.dataset.index;
+
+  todos[todoDeleteIndex].isDone = !todos[todoDeleteIndex].isDone;
+
+  drawToDoList();
+}
+
+function addTodo(event) {
+  event.preventDefault();
+
+  fromForm.value;
+
+ // todos.push(fromForm.value)
+
+ todos.push(
+    {
+      text: fromForm.value,
+      isDone: false,
+    }
+ )
+
+  console.log(todos);
+
+  form.reset();
+
+  drawToDoList();
+
+}
+
+form.addEventListener("submit", addTodo);
 //
 //  JS File
 //  You may remove the code below - it's just boilerplate
